44093f85f4b8cca4ed9265b310e7d8c27202a8bf
mingyeongho <1736s@naver.com>
2021-08-04 01:50:10 +0900

    regex

--- src/Components/Login/Join/Join.js
+++ src/Components/Login/Join/Join.js
@@ -1,6 +1,65 @@
 import { useRef, useState } from "react";
 import "./Join.scss";
 
+const depart = {
+  융합기술대학: [
+    "기계자동자항공공학부",
+    "기계공학전공",
+    "자동차공학전공",
+    "항공기계설계전공",
+    "전기공학전공",
+    "전자공학과",
+    "의료IT공학전공",
+    "컴퓨터학부",
+    "컴퓨터공학전공",
+    "소프트웨어전공",
+    "AI로봇공학과",
+    "바이오메디컬융합학과",
+  ],
+  공과대학: [
+    "건설환경도시교통공학부",
+    "사회기반공학전공",
+    "환경공학전공",
+    "도시교통공학전공",
+    "응용화학에너지공합부",
+    "화공생물공학전공",
+    "에너지소재공학전공",
+    "나노화학소재공학전공",
+    "산업경영안전공학부",
+    "산업경영공학전공",
+    "안전공학전공",
+    "건축학부",
+    "건축공학전공",
+    "건축학전공",
+    "디자인학부",
+    "산업디자인전공",
+    "커뮤니케이션디자인전공",
+  ],
+  인문사회대학: [
+    "글로벌어문학부",
+    "영어영문학전공",
+    "비즈니스영어전공",
+    "중국어전공",
+    "한국어문학전공",
+    "행정학부",
+    "행정학전공",
+    "행정정보학전공",
+    "경영통상복지학부",
+    "경영학전공",
+    "융합경영전공",
+    "국제통상전공",
+    "사회복지학전공",
+    "음악학과",
+    "스포츠학부",
+    "스포츠의학전공",
+    "스포츠산업학전공",
+    "항공서비스학과",
+    "항공운항학과",
+    "유아교육학과",
+    "미디어컨텐츠학과",
+  ],
+};
+
 const Join = ({ history }) => {
   const [inputs, setInputs] = useState({
     id: "",
@@ -8,8 +67,9 @@ const Join = ({ history }) => {
     name: "",
     email: "",
     department: "",
+    club: "",
   });
-  const { id, pw, name, email, department } = inputs;
+  const { id, pw, name, email, department, club } = inputs;
 
   const onChange = (e) => {
     setInputs({
@@ -28,8 +88,14 @@ const Join = ({ history }) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("회원가입");
+    console.log({ inputs });
+    const idReg = /\d{7}/;
+    if (!idReg.test(id)) {
+      alert("아이디가 잘못되었습니다.");
+    }
   };
+
+  // console.log(depart);
 
   return (
     <div className="joinPage">
@@ -45,8 +111,10 @@ const Join = ({ history }) => {
             id="id"
             autoFocus
             minLength="7"
+            maxLength="7"
+            required
             className="input"
-            placeholder="예)1234567"
+            placeholder="예) 1234567"
             value={id}
             onChange={onChange}
           />
@@ -59,6 +127,7 @@ const Join = ({ history }) => {
             name="pw"
             ref={inputPw}
             id="pw"
+            required
             className="input"
             placeholder="비밀번호를 입력해주세요"
             value={pw}
@@ -72,6 +141,7 @@ const Join = ({ history }) => {
             type="text"
             name="name"
             id="name"
+            required
             className="input"
             placeholder="이름을 입력해주세요."
             value={name}
@@ -84,27 +154,39 @@ const Join = ({ history }) => {
             type="email"
             name="email"
             id="email"
+            required
             className="input"
-            placeholder="예)hong@naver.com"
+            placeholder="예) hong@naver.com"
             value={email}
             onChange={onChange}
           />
           <label htmlFor="department" className="text">
             학과
           </label>
-          <input
-            type="text"
+          <select
             name="department"
             id="department"
             className="input"
-            placeholder="예)소프트웨어학과"
+            required
             value={department}
             onChange={onChange}
-          />
-
-          <button type="submit">회원가입</button>
+          ></select>
+          <label htmlFor="club" className="text">
+            동아리
+          </label>
+          <select
+            name="club"
+            id="club"
+            className="input"
+            required
+            value={club}
+            onChange={onChange}
+          ></select>
+          <button type="submit" className="submitBtn">
+            회원가입
+          </button>
         </form>
-        <button onClick={() => history.push("/")}>돌아가기</button>
+        {/* <button onClick={() => history.push("/")}>돌아가기</button> */}
       </div>
     </div>
   );
