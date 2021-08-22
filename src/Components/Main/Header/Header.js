41e5f3ec20c499ec8b8c0b073bc59c803d7fdced
mingyeongho <1736s@naver.com>
2021-08-22 16:43:21 +0900

    Main의 Header

---
+++ src/Components/Main/Header/Header.js
@@ -0,0 +1,21 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header_wrapper">
+      <div className="header_img">
+        <div className="header_inner">
+          <div className="header_text">
+            <strong className="main_text">
+              KOREA NATIONAL UNIVERSITY OF TRANSPORTATION
+            </strong>
+            <br />
+            <strong className="sub_text">대학생활</strong>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
