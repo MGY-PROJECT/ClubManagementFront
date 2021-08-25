3061a4bb7ee1673018dc1ae8cf810c98916f779d
mingyeongho <1736s@naver.com>
2021-08-26 02:13:38 +0900

    Explanation

--- src/Components/Main/Section/Explanation/Explanation.js
+++ src/Components/Main/Section/Explanation/Explanation.js
@@ -3,22 +3,22 @@ import "./Explanation.scss";
 const Explanation = () => {
   return (
     <div className="explanation">
-      <div className="enrollment">
+      <div className="enrollment block">
         <i className="fas fa-user"></i>
         <div className="text">
           <h2>동아리 등록</h2>
-          <h5>
-            총 동아리 연합회에서 동아리 등록신청서를 교부 받아 총 동아리
+          <div className="sub">
+            총 동아리 연합회에서 동아리 등록 신청서를 교부받아 총 동아리
             연합회에 등록
-          </h5>
-          <h5>
+          </div>
+          <div className="sub">
             등록 서류 : 동아리 등록 신청서, 회원명부, 회칙, 사업계획서,
             발기취기문, 동아리서약서, 타 동아리 대표자 10인 이상의 추진서,
             회원의 입회원서 사본
-          </h5>
+          </div>
         </div>
       </div>
-      <div className="call">
+      <div className="call block">
         <i className="fas fa-phone"></i>
         <div className="text">
           <h2>연락처</h2>
